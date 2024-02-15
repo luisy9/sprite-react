9635a2cc41b18005ba6130b0d7bfa7e621edfd52
Luis de Haro <luisdeharo4@gmail.com>
2024-02-15 11:18:45 +0100

    quit bug

--- src/components/Celda.jsx
+++ src/components/Celda.jsx
@@ -1,9 +1,5 @@
 const Celda = ({ clickCell, m, x, mida, canvas }) => {
 
-  const color = canvas[m];
-
-  console.log(color)
-
   return (
     <div className="w-10 h-10 cursor-pointer 
     border-2 border-solid" style={{ backgroundColor: m }}
